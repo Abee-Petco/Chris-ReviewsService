53326b44a240115a10f573eb8bd4d56520ed7fcf
Chris Hesterman <chrishesterman14@gmail.com>
2020-10-04 15:53:14 -0700

    More adjustments for deployment
    add port to db connection file, change so called 'environmental variable'

--- db/index.js
+++ db/index.js
@@ -4,7 +4,8 @@ const pool = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_IP,
-  database: process.env.DATABASE_NAME
+  database: process.env.DATABASE_NAME,
+  port: 5432
 });
 
 module.exports = pool;
